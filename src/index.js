5f7b114f3a586faa7d8a1a2cac1d5e9716f1eff5
Joy Tari-bagshaw <111312788+JoyTaribagshaw@users.noreply.github.com>
2023-07-26 21:21:31 +0100

    Update index.js file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 import './index.css';
+import { createScore, fetchScore, addScore } from './modules/api.js';
+
+const refreshBtn = document.querySelector('.reset-btn');
+const addBtn = document.querySelector('.add-btn');
+
+refreshBtn.addEventListener('click', async () => {
+  const scoreList = document.querySelector('.score-list');
+  const scores = await fetchScore();
+  scoreList.innerHTML = '';
+  scores.forEach((n) => {
+    scoreList.appendChild(createScore(n.user, n.score));
+  });
+});
+
+addBtn.addEventListener('click', async (e) => {
+  e.preventDefault();
+  const form = document.querySelector('form');
+  const name = document.getElementById('name').value;
+  const score = document.getElementById('score').value;
+  await addScore(name, score);
+  form.reset();
+});
+
